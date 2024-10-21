77a2877ad398d2f1c3fa152dda55cf36e20c9238
Camper-code <57728580+Camper-code@users.noreply.github.com>
2024-10-21 20:17:50 +0300

    [1.0.1] Fixed installing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-# [0.0.1] Unity Message Box - 10-21-2024
+# [1.0.1] Unity Message Box - 10-21-2024
+What's new:
+- Fixed installing error
+
+# [1.0.0] Unity Message Box - 10-21-2024
 What's new:
 - Added base functional
--- Runtime/MessageBox.cs
+++ Runtime/MessageBox.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NativeDialog;
-
 namespace CampStudio.MessageBox
 {
     public static partial class MessageBox
@@ -12,8 +10,8 @@ namespace CampStudio.MessageBox
             #if UNITY_EDITOR
                 UnityEditor.EditorUtility.DisplayDialog(tittle, text, "OK");
             #else
-                DialogManager.SetLabel("OK", "Ok", "Ok");
-                DialogManager.ShowSubmit(text, text, _ => {});
+                NativeDialog.DialogManager.SetLabel("OK", "Ok", "Ok");
+                NativeDialog.DialogManager.ShowSubmit(text, text, _ => {});
             #endif
         }
     }
